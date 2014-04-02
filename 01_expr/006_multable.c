d0f1992749a98b6cdf6ac18add0789bf9e05fbbb
SAS118 CCT <sas118cct@gmail.com>
2014-04-02 21:00:06 +0530

    Multiplication table

---
+++ 01_expr/006_multable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i=1,j,p,m,sum;	
+	printf("Enter any number");
+	scanf("%d", &a);
+	m = 1;	
+	for(i=1;i<=a;i++){
+	for(j=1;j<=b;j++){
+	sum = 1;
+	}
+	p = sum*m;
+	}
+}

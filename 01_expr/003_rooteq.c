1c266ff9bb093f9b976ca2d43e282c92bf85f825
SAS118 CCT <sas118cct@gmail.com>
2014-04-02 09:58:33 +0530

    square root

---
+++ 01_expr/003_rooteq.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c,r1,r2,t;
+	printf("Enter the values of a,b,c");
+	scanf("%d %d %d", &a,&b,&c);
+	t = sqrt(b*b-4*a*c);
+	r1 = (-b+t)/(2*a);
+	r2 = (-b-t)/(2*a);
+	printf("The quadratic roots are %d %d", r1,r2);
+	return 0;
+}
+

54b221ca9eec6ef0434e4f03ba127901b6f5ac5a
SAS118 CCT <sas118cct@gmail.com>
2014-04-02 10:49:41 +0530

    Case change

---
+++ 03_strings/001_casehange.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[10];
+	printf("Enter a string in uppercase:");
+	scanf("%s",&a[10]);
+	printf("The string in lowercase is:%s",  strlwr(a));
+	return 0;
+}

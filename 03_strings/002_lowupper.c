bc08f2f27f5298f99562fe7f119e79b95cd05263
sas118 cct <sas118cct@gmail.com>
2014-04-07 12:50:16 +0530

    LCM of two numbers

--- 03_strings/002_lowupper.c
+++ 03_strings/002_lowupper.c
@@ -2,11 +2,11 @@
 int main()
 {
 	char s[10], i;
-	printf("Enter any string in lower case;");
-	scanf("%s", s[10]);
-	for(i=0;i<=10;i++){
+	printf("Enter any string in lower case");
+	scanf("%s", s);
+	for(i=0;i<10&&s[i]!='\0';i++){
 		s[i] = s[i]-32;
 	}
-	printf("%s", s);
+	printf("%s\n", s);
 }
 

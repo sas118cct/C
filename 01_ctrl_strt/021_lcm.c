41855614bb1dd8caa5bc11cc84f149f9a2c8a0cd
sas118 cct <sas118cct@gmail.com>
2014-04-07 12:56:29 +0530

    lcm  bug fixed Incorrect operator was used

--- 01_ctrl_strt/021_lcm.c
+++ 01_ctrl_strt/021_lcm.c
@@ -2,16 +2,16 @@
 int main()
 {
 	int i,j,a,b;
-	printf("Enter two numbers:");
-	scanf("%d %d", &a,&b);
-	if(a == b){
+	printf("Enter the two numbers\n");
+	scanf("%d %d", &a,&b);	
+	if(a==b){
 	printf("%d is the lcm", a);
 	}
 	else{
 	for(i=1;i<=a*b;i++){
-		if(i/a==0 && i/b==0 && a*b/i==0)
+		if(i%a==0 && i%b==0 && a*b%i==0)
 			printf("%d is the lcm of the given number", i);
-			}
+		}
 	}
 	return 0;
 }

5544c0a09830d2ee7ee214c7c6416aea11690212
sas118 cct <sas118cct@gmail.com>
2014-04-07 11:10:15 +0530

    To show the virtual address space for a process

---
+++ 06_pointers/005_virtualadd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,*i = a;
+	printf("Program to make system wait for input\n\n");
+	printf("Type the commands follows\n");
+	printf("ps -a\n cat /proc/PID/status\n cat /proc/PID/maps\n");
+	scanf("%d",&a);
+	return 0;
+}
+

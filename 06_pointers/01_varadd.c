83e416793dbf176bd4d20f792c476b1d97fda83e
sas118 cct <sas118cct@gmail.com>
2014-04-07 09:52:42 +0530

    Accessing address of different data types and values via indirection

--- 06_pointers/01_varadd.c
+++ 06_pointers/01_varadd.c
@@ -6,10 +6,10 @@ int main()
 	float f, *ptr3 = &f;
 	double d, *ptr4 = &d;
 	i = 10; c = 'A'; f = 10.5; d = 11.6;
-	printf("The address of %d is %p\n", i,&ptr1);
-	printf("Teh addres of %c is %p\n", c,&ptr2);
-	printf("The address of %f is %p\n", f,&ptr3);
-	printf("The address of %f is %p\n", f,&ptr4);
+	printf("The address of i:%d is %p. value:%d\n", i, &ptr1, *ptr1);
+	printf("The addres of c:%c is %p. value:%c\n", c, &ptr2, *ptr2);
+	printf("The address of f:%f is %p. value:%f\n", f, &ptr3, *ptr3);
+	printf("The address of d:%f is %p. value:%f\n", d, &ptr4, *ptr4) ;
 	return 0;
 }
 

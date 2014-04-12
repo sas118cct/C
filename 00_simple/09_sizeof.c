33b31e89a3177f33b79e06719917eed85dc2aa8e
Maruthi Seshidhar <maruthi.seshidhar@gmail.com>
2014-04-12 06:18:21 +0530

    Sizes of basic, signed/unsgined, short/long types.

--- 00_simple/09_sizeof.c
+++ 00_simple/09_sizeof.c
@@ -1,10 +1,32 @@
 #include<stdio.h>
 int main()
 {
-	int i;
-	char c; float f; double d;
-	int I = sizeof(i), C = sizeof(c), F = sizeof(f), D = sizeof(d);
-	printf("isize = %d  csize = %d  fsize = %d  dsize = %d", I,C,F,D);
+	printf("sizeof(char):%d\n", sizeof(char));
+	printf("sizeof(int):%d\n", sizeof(int));
+	printf("sizeof(float):%d\n", sizeof(float));
+	printf("sizeof(double):%d\n", sizeof(double));
+	printf("\n");
+	printf("\n");
+
+	printf("sizeof(unsigned char):%d\n", sizeof(unsigned char));
+	printf("sizeof(signed char):%d\n", sizeof(signed char));
+	printf("\n");
+
+	printf("sizeof(unsigned int):%d\n", sizeof(unsigned int));
+	printf("sizeof(signed int):%d\n", sizeof(signed int));
+	printf("\n");
+	printf("\n");
+
+	printf("sizeof(short int):%d\n", sizeof(short int));
+	printf("sizeof(long int):%d\n", sizeof(long int));
+	printf("sizeof(long long int):%d\n", sizeof(long long int));
+	printf("\n");
+	printf("\n");
+
+	printf("sizeof(long double):%d\n", sizeof(long double));
+	printf("\n");
+	printf("\n");
+
 	return 0;
 }
 

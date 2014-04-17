95f689364e95eef56e7b6da7b7d98240746562de
SAS118 CCT <sas118cct@gmail.com>
2014-04-17 18:42:06 +0530

    Changes done

--- 06_pointers/003_pointersize.c
+++ 06_pointers/003_pointersize.c
@@ -6,9 +6,9 @@ int main()
 	char c, *ptr2 = &c;
 	float f,*ptr3 = &f;
 	double d,*ptr4 = &d;
-	printf("Size of i is %p and Size of ptr1 is %p\n\n", sizeof(i), sizeof(ptr1));
-	printf("Size of c is %p and Size of ptr2 is %p\n\n", sizeof(c), sizeof(ptr2));
-	printf("Size of f is %p and Size of ptr3 is %p\n\n", sizeof(f), sizeof(ptr3));
-	printf("Size of d is %p and Size of ptr4 is %p\n\n", sizeof(d), sizeof(ptr4));
+	printf("Size of i is %d and Size of ptr1 is %d\n", sizeof(i), sizeof(ptr1));
+	printf("Size of c is %d and Size of ptr2 is %d\n", sizeof(c), sizeof(ptr2));
+	printf("Size of f is %d and Size of ptr3 is %d\n", sizeof(f), sizeof(ptr3));
+	printf("Size of d is %d and Size of ptr4 is %d\n", sizeof(d), sizeof(ptr4));
 	return 0;
 }

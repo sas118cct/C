434e4739e5bc0790a1aefaca991a8651ff03a38b
SAS118 CCT <sas118cct@gmail.com>
2014-04-17 07:00:47 +0530

    Changes done

--- 00_simple/08_interest.c
+++ 00_simple/08_interest.c
@@ -3,12 +3,12 @@
 int main()
 {
 	float p,n,r,a,s,c;
-	printf("Enter the amount and number of months");
+	printf("Enter the amount and number of months\n");
 	scanf("%f %f", &p,&n);
 	r = 0.01; s = (p*n*r)/100;
 	printf("The simple intrest is %f \n ", s);
 	c = p * pow((1+r/100),n);
-	printf("The compound intrest is %f ", c);
+	printf("The compound intrest is %f\n", c);
 	return 0;
 }
 

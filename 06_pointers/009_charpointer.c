7105b1227ee5cfbf394e90c90b9fb6944641d4c8
SAS118 CCT <sas118cct@gmail.com>
2014-04-17 18:53:24 +0530

    Changes done

--- 06_pointers/009_charpointer.c
+++ 06_pointers/009_charpointer.c
@@ -3,8 +3,7 @@ int main()
 {
 	char c[10]="HELLO",*cp,i;
  	for(i=0,cp=&c[0];i<10;i++){
-        printf("c[%d]:%c is *(%p):%d\n", i, c[i], cp, *cp);
-        cp = cp+1;
+        printf("c[%d]:%c is *(%p):%d\n", i, c[i], (cp+1), (*cp+1));
         }
         return 0;
 }

9a3538ad65b3b7b1a2e19cbbe8cb81bb4d23d13f
SAS118 CCT <sas118cct@gmail.com>
2014-04-17 06:57:23 +0530

    Changes done

--- 00_simple/05_areatri.c
+++ 00_simple/05_areatri.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<math.h>
 
 int main()
 {
-	int a,b,c,s,area;
+	float a,b,c,s,area;
 	printf("Enter the lengths of sides\n");
-	scanf("%d %d %d", &a, &b, &c);
+	scanf("%f %f %f", &a, &b, &c);
 	s = a+b+c;
 	area = sqrt(s*(s-a)*(s-b)*(s-c));
 	printf("The area of the triangle is %d\n\n", area);

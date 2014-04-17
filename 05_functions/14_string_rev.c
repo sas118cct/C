485d582bd1e95354dca56fd92edf65f685efc73d
SAS118 CCT <sas118cct@gmail.com>
2014-04-17 18:09:00 +0530

    Changes made

--- 05_functions/14_string_rev.c
+++ 05_functions/14_string_rev.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
  
-void reverse(char [], int, int);
+void reverse(char [],int);
 int main()
 {
-    char str1[20];
-    int size;
+    	char str1[20];
+    	int size;
  
-    printf("Enter a string to reverse: ");
-    scanf("%s", str1);
-    size = strlen(str1);
-    reverse(str1, 0, size - 1);
-    printf("The string after reversing is: %s\n", str1);
-    return 0;
+    	printf("Enter a string to reverse: ");
+    	scanf("%s", str1);
+    	size = strlen(str1);
+    	reverse(str1,0);
+	printf("\n");
+    	return 0;
 }
+
  
-void reverse(char str1[], int num, int size)
+void reverse(char str[],int i)
 {
-    char path;
-    path = str1[num];
-    str1[num] = str1[size - num];
-    str1[size - num] = path;
-    if (num == size / 2)
-    {
-        return;
-    }
-    reverse(str1, num + 1, size);
+	if (str[i] == '\0')
+		return;
+	else
+		reverse(str,i+1);
+	printf("%c", str[i]);
+
 }

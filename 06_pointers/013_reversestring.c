eb4777edfb55267451eae7ac396a1905fc21a1a7
SAS118 CCT <sas118cct@gmail.com>
2014-04-18 08:05:19 +0530

    Changes done

--- 06_pointers/013_reversestring.c
+++ 06_pointers/013_reversestring.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
 int main()
 {
-	char str[100], p;
+	char str[100], p,(*ptr)=&str[100];
 	int i,j,n,k;
 	printf("Enter the string\n");
 	scanf("%s",str);
 	i = 0;
-	while(i<100 && str[i]!='\0'){
+	while(i<100 && (ptr+i)!='\0'){
 	i+=1;
 	}
 	n = i;
-	for(j=0;j<=n/2;j++){
-		p = str[j];
-		str[j] = str[n-j];
-		str[n-j] = p;
-	}
 	printf("The reversed string is \n");
+	for(j=0;j<=n/2;j++){
+		p = *(ptr+j);
+		*(ptr+j) = *(ptr+(n-j));
+		*(ptr+(n-j)) = p;
+
+	}
 	printf("%s", str);
+
 	return 0;
 }
 
